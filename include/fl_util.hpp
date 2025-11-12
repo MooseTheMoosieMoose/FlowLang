724e9e21dd5841e7345cb33d275b209a41466379
MooseTheMoosieMoose <moosedeen333@yahoo.com>
2025-11-12 10:29:30 -0800

    Initial design of custom span type
    Added a custom span type, as well as a default templated contigous iterator for downstream implementation into other components

--- include/fl_util.hpp
+++ include/fl_util.hpp
@@ -10,6 +10,9 @@ This software is licensed under the BSD 3-Clause License, which can be found in 
 
 #include "utf8string.hpp"
 #include <variant> //This could be abandoned in favor of tagged unions but ehhhh
+#include <stdexcept> //need this to throw runtime errors
+#include <algorithm> //Gets us all sorts of goodies
+#include <iterator> //Access to custom iterators for the span type
 #include <type_traits>
 
 namespace fl {
@@ -87,25 +90,174 @@ private:
 };
 
 /*======================================================================================================*/
+/*                                     Contiguse Iter                                                  */
+/*======================================================================================================*/
+
+/**
+ * @brief a contigous iter is an iterator over a contigous type, which allows it to be optimized for many
+ * algorithms inside the standard library. Most data types worked with in this project are continuous so
+ * providing this iter makes it easier to implement iters
+ */
+template <typename T>
+class ContIter {
+public:
+    //Basic using traits
+    using iterator_catagory = std::random_access_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer = T*;
+    using reference = T&;
+
+    //Constructor for begin and end
+    ContIter(pointer ptr) noexcept : iptr(ptr) {}
+
+    //Define for weakly_incrementable
+    ContIter& operator++() { iptr++; return *this; }
+    ContIter operator++(int) {ContIter temp = *this; (*this)++; return temp; }
+    ContIter() noexcept : iptr(nullptr) {}
+
+    //Define for input_or_output_iterator
+    reference operator*() { return *iptr; }
+
+    //Define for indirectly_readable
+    friend reference operator*(const ContIter& it) { return *(it.iptr); }
+
+    //Define for forward_iterator
+    bool operator==(const ContIter& it) const { return iptr == it.iptr; }
+
+    //Define for bi-directional iterator
+    ContIter& operator--() { iptr--; return *this; }
+    ContIter operator--(int) { ContIter tmp = *this; (*this)--; return tmp; }
+
+    //Define for random access iterator
+    bool operator!=(const ContIter& it) const noexcept { return iptr != it.iptr; }
+    bool operator<(const ContIter& it) const noexcept { return iptr < it.iptr; }
+    bool operator>(const ContIter& it) const noexcept { return iptr > it.iptr; }
+    bool operator<=(const ContIter& it) const noexcept { return iptr <= it.iptr; }
+    bool operator>=(const ContIter& it) const noexcept { return iptr >= it.iptr; }
+
+    difference_type operator-(const ContIter& it) const { return iptr - it.iptr; }
+
+    //Define for iter_difference
+    ContIter& operator+=(difference_type diff) { iptr += diff; return *this; }
+    ContIter& operator-=(difference_type diff) { iptr -= diff; return *this; }
+    ContIter operator+(difference_type diff) const { return ContIter(iptr + diff); }
+    ContIter operator-(difference_type diff) const { return ContIter(iptr - diff); }
+
+    friend ContIter operator+(difference_type diff, const ContIter& it) {
+        return it + diff;
+    }
+
+    friend ContIter operator-(difference_type diff, const ContIter& it) {
+        return it - diff;
+    }
+
+    reference operator[](difference_type diff) const { return iptr[diff]; }
+
+private:
+    //Our internal iter pointer
+    T* iptr;
+};
+
+/*======================================================================================================*/
 /*                                          Span                                                        */
 /*======================================================================================================*/
 
 /**
  * @brief provides a non-owning view over contiguous data, meant to be a drop in
  * of C++20s std::span
+ * @todo for certain specializations, check to implement something like a toOwned()
+ * for saftey
  */
 template <typename T>
 class Span {
 public:
+    //Attach the iterator
+    using element_type = T;
+    using value_type = std::remove_cv_t<T>;
+    using size_type = size_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using iterator = ContIter<T>;
+    using const_iterator = ContIter<const T>;
+
     /**
      * @brief the empty, uninitilzed default span constructor
      */
-    Span() : ptr(nullptr), len(0) {}
+    Span() noexcept : ptr(nullptr), len(0) {}
 
     /**
      * @brief a basic constructor for a general span
      */
-    Span(T* first, size_t count) : ptr(first), len(count) {}
+    Span(T* first, size_t count) noexcept : ptr(first), len(count) {}
+
+    /**
+     * @brief index operator for non-const access
+     */
+    T& operator[] (size_t index) {
+        if (index > len) {
+            throw std::runtime_error("Illegal index access to span!");
+        }
+        return ptr[index];
+    }
+
+    /**
+     * @brief index operator for const access
+     */
+    const T& operator[] (size_t index) const {
+        if (index > len) {
+            throw std::runtime_error("Illegal index access to span!");
+        }
+        return ptr[index];
+    }
+
+    /**
+     * @brief operator overload for equality, checks to see if the data inside the span
+     * is the same, not if the internal pointers are the same
+     */
+    bool operator==(const Span& other) const {
+        return (len != other.len) ? false : std::equal(ptr, (ptr + len), other.ptr, (other.ptr + other.len)); 
+    }
+
+    /**
+     * @brief provides a begin() function for algorithm work, by attaching its iter
+     */
+    iterator begin() const noexcept {
+        return iterator(ptr);
+    }
+
+    /**
+     * @brief provides an end() function for the rest of the iterator attachment
+     */
+    iterator end() const noexcept {
+        return iterator(ptr + len);
+    }
+
+    /**
+     * @brief provides a constant iterator beign for the span
+     */
+    const_iterator cbegin() const noexcept {
+        return const_iterator(ptr);
+    }
+
+    /**
+     * @brief provides a constant iterator end for the span
+     */
+    const_iterator cend() const noexcept {
+        return const_iterator(ptr + len);
+    }
+
+    /**
+     * @brief creates a subspan from `ptr + startIndx` to `ptr + endIndx`
+     * @warning startIndx should be less than endIndx!
+     */
+    Span subspan(size_t startIndx, size_t endIndx) {
+        return Span((ptr + startIndx), endIndx);
+    }
+
 private:
     //Base data pointer
     T* ptr;
--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,23 @@
 using namespace fl;
 
 int main() {
-    Utf8String::setLocale();
-    std::string filePath = "/mnt/c/Users/Moose/Desktop/Programming/FlowLang/test.fl";
-    Utf8String fileContent = Utf8String::fromFile(filePath.c_str());
-    Tokenizer tokenizer = Tokenizer(fileContent);
-    // for (auto t : tokenizer.getTokens()) {
-    //     std::cout << t << std::endl;
-    // }
-    auto tokens = tokenizer.getTokens();
-    ASTNodePtr head = parseGlobal(std::span<Token>(tokens.begin(), tokens.end())).value();
+    // Utf8String::setLocale();
+    // std::string filePath = "/mnt/c/Users/Moose/Desktop/Programming/FlowLang/test.fl";
+    // Utf8String fileContent = Utf8String::fromFile(filePath.c_str());
+    // Tokenizer tokenizer = Tokenizer(fileContent);
+    // // for (auto t : tokenizer.getTokens()) {
+    // //     std::cout << t << std::endl;
+    // // }
+    // auto tokens = tokenizer.getTokens();
+    // ASTNodePtr head = parseGlobal(std::span<Token>(tokens.begin(), tokens.end())).value();
 
-    auto res = fl::Result<int, double>::Ok(4);
+    // auto res = fl::Result<int, double>::Ok(4);
+
+    std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto subspan = fl::Span<int>(nums.data() + 2, 3);
+    for (const auto& num : subspan) {
+        std::cout << "Span over element: " << num << std::endl;
+    }
+
     return 0;
 }
